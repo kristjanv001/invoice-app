4a91550ec65d0054fb73f362c5df502ef7f919bd
Kristjan Vingel <kristjan.vingel.protonmail.ch>
2023-10-28 13:03:27 +0700

    invoice card done

--- app/components/InvoicesList.tsx
+++ app/components/InvoicesList.tsx
@@ -5,7 +5,7 @@ import { Invoice } from "../interfaces/invoice";
 interface InvoicesListProps { invoices: Invoice[] }
 export function InvoicesList({invoices}: InvoicesListProps) {
   return (
-    <ul>
+    <ul className="mb-24">
       {invoices.map((invoice) => {
           return (
             <li key={invoice.id}>
